c10c4049884a9ca11550f302ccdab5aa0141a685
= <justinclaveria21@gmail.com>
2023-08-20 22:38:40 +0800

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import { Link, type HeadFC, type PageProps } from "gatsby"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import Layout from "@/components/core/layout"
+
+
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <Layout>
+      Hello World s s
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home Page</title>
